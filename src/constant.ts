29141f613d804ae376cc0e623592d86cfae149b0
Thoobi <sydneydaniel76@gmail.com>
2026-01-13 14:48:17 +0100

    feat: update API rewrites for Webflow integration and clean up unused constants

--- src/constant.ts
+++ src/constant.ts
@@ -1,5 +1,3 @@
 export const BASE_URL = import.meta.env.VITE_WEBFLOW_URL;
-// export const WEBFLOW_TOKEN = import.meta.env.VITE_WEBFLOW_TOKEN;
 export const WORKSPACE_TOKEN = import.meta.env.VITE_WEBFLOW_WORKSPACE_TOKEN;
-// export const SITE_ID = import.meta.env.VITE_WEBFLOW_SITE_ID;
 export const COLLECTION_ID = import.meta.env.VITE_WEBFLOW_COLLECTION_ID;
